b1206049592288d9d2f94fe7bfdf2940bc7827b6
hmanjun <harshith.hm03@gmail.com>
2022-12-31 18:11:44 -0800

    Add framework for express: middleware + view engine(react)

--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
 const express = require("express")
 const db = require("./config/connection")
+const path = require('path')
+const ws = require("ws")
+const http = require("http")
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+//Middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//Render React html
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+}
+  
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+//Create websocket server
+const server = new http.createServer(app)
+const wss = new WebSocket.Server({server})
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`)
+    })
+})
