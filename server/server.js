7964fad6ad898af5f3a8237873ef37cb78001b80
hmanjun <harshith.hm03@gmail.com>
2023-01-01 17:32:59 -0800

    Add session parser and base wss listeners

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express")
-const db = require("./config/connection")
+const session = require('express-session')
+const uuid = require('uuid')
 const path = require('path')
 const ws = require("ws")
 const http = require("http")
 
-const PORT = process.env.PORT || 3001
+const db = require("./config/connection")
+const PORT = 8080
+
+//const PORT = process.env.PORT || 3001
 const app = express()
+const sess = {
+    secret: 'fnwafnj dwabi',
+    saveUninitialized: false,
+    resave:false
+}
+app.use(session(sess))
 
 //Middleware to parse incoming data
 app.use(express.urlencoded({ extended: true }))
@@ -20,12 +30,34 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
+//Save ws with key userId
+const map = {}
+
 //Create websocket server
 const server = new http.createServer(app)
-const wss = new WebSocket.Server({server})
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true })
+
+//Server middleware, checking login status should be added here later
+server.on('upgrade', (req, socket, head) => { 
+    console.log('Bypassing check of session data')
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    })
+})
+
+//WSS listeners
+wss.on("connection", (ws,req) => {
+    ws.on('message', message =>{
+        console.log(`Recieved message: ${message}`)
+    })
+
+    ws.on('close', () => {
+        //map.delete(userId)
+    })
+})
 
 db.once('open', () => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`)
     })
 })
