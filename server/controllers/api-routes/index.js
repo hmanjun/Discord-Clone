43fa327cbadf52d776b66d553d6facd88ec1b0c6
hmanjun <harshith.hm03@gmail.com>
2023-01-07 13:23:56 -0800

    Reorganized routes and moved routes to use api routing

--- server/controllers/api-routes/index.js
+++ server/controllers/api-routes/index.js
@@ -1,51 +1,9 @@
 const router = require('express').Router()
-const {User} = require('../../models')
 
-router.post('/sign-up', async(req, res) => {
-    try {
-        const {username, email, password} = req.body
-        const userData = User.create({username, email, password})
-        
-        req.session.save(() => {
-            req.session.userId = userData._id
-            req.session.logged_in = true
-            res.status(200).json({message: `New user was successfully created.`})
-        })
-    } catch (err) {
-        res.status(400).json(err)
-    }
-})
+const userRoutes = require('./user-routes')
+const channelRoutes = require('./channel-routes')
 
-router.post('/login', async(req,res) => {
-    try {
-        const userData = User.findOne({email: req.body.email})
-        if(!userData){
-            res.status(400).json({message: `No user with that email and password was found.`})
-            return
-        }
-        const validPassword = await userData.isCorrectPassword(req.body.password)
-        if(!validPassword) {
-            res.status(400).json({message: `No user with that email and password was found.`})
-            return
-        }
-        req.session.save(() => {
-            req.session.userId = userData._id
-            req.session.logged_in = true
-            res.status(200).json({message: `User id was saved to session.`})
-        })
-    } catch (err) {
-        res.status(400).json(err)
-    }
-})
+router.use('/user', userRoutes)
+router.use('/channel', channelRoutes)
 
-router.post('/logout', async(req,res) => {
-    try {
-        if(req.session.logged_in){
-            req.session.destroy(() => {
-                res.status(204).end()
-            })
-        } else res.status(400).end()
-    } catch (err) {
-        res.status(400).json(err)
-    }
-})
+module.exports = router
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
-
-const channelRoutes = require('./channelRoutes')
 const apiRoutes = require('./api-routes')
 
 router.use('/api', apiRoutes)
-router.use('/channels', channelRoutes)
 
 module.exports = router
