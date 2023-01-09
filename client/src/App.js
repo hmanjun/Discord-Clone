6d0f26c0cf45e1346bed744c570d26d3ee3abd88
hmanjun <harshith.hm03@gmail.com>
2023-01-09 11:18:53 -0800

    Add register page with proper backend linking + styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,22 @@
 import './App.css';
 import React, {useState} from 'react';
-let ws
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
 
 function App() {
-  const [roomName, setRoomName] = useState("")
-  const [recievedMessage, setRecievedMessage] = useState([])
-  const [sendMessage, setSendMessage] = useState("")
-  const [connected, setConnection] = useState(false)
   
-  const connectWss = () =>{
-    if(connected) return
-    if(ws){
-      ws.onerror = ws.onopen = ws.onclose = null;
-      ws.close();
-    }
-
-    ws = new WebSocket(`ws://${window.location.host}`)
-
-    ws.onopen = () =>{
-      setConnection(true)
-      ws.send(JSON.stringify({joinRoom: true, roomName: `${roomName}`}))
-    }
-
-    ws.addEventListener('message', (message) => {
-      const {data} = message
-      setRecievedMessage(curr => [...curr, data])
-    })
-  }
-
-  const send = () => {
-    if(!ws) return
-    console.log(`sending: ${sendMessage}`)
-    ws.send(JSON.stringify({joingRoom: false, message: `${sendMessage}`}))
-    setSendMessage("")
-
-  }
-
   return (
     <div className="App">
-      <h1>Connected status: {connected ? `connected` : `disconnected`}</h1>
-      <h3>Room Code: <input value={roomName} onChange={e => setRoomName(e.target.value)}></input></h3>
-      <button onClick={connectWss}>Connect to Server</button>
-      <h2>Recent Messages</h2>
-      <div className='recieved-messages'>{recievedMessage.map(message => (<h4>{message}</h4>))}</div>
-      <input className='input-bar' value={sendMessage} onChange={e => setSendMessage(e.target.value)}></input>
-      <button className='send-message-button' onClick={send}>Send</button>
+      <Router>
+        <Routes>
+          <Route
+            path="/register"
+            element={<RegisterPage/>}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/testPage.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+let ws
+
+function TestPage() {
+  const [roomName, setRoomName] = useState("")
+  const [recievedMessage, setRecievedMessage] = useState([])
+  const [sendMessage, setSendMessage] = useState("")
+  const [connected, setConnection] = useState(false)
+  
+  const connectWss = () =>{
+    if(connected) return
+    if(ws){
+      ws.onerror = ws.onopen = ws.onclose = null;
+      ws.close();
+    }
+
+    ws = new WebSocket(`ws://${window.location.host}`)
+
+    ws.onopen = () =>{
+      setConnection(true)
+      ws.send(JSON.stringify({joinRoom: true, roomName: `${roomName}`}))
+    }
+
+    ws.addEventListener('message', (message) => {
+      const {data} = message
+      setRecievedMessage(curr => [...curr, data])
+    })
+  }
+
+  const send = () => {
+    if(!ws) return
+    console.log(`sending: ${sendMessage}`)
+    ws.send(JSON.stringify({joingRoom: false, message: `${sendMessage}`}))
+    setSendMessage("")
+
+  }
+
+  return (
+    <div className="App">
+      <h1>Connected status: {connected ? `connected` : `disconnected`}</h1>
+      <h3>Room Code: <input value={roomName} onChange={e => setRoomName(e.target.value)}></input></h3>
+      <button onClick={connectWss}>Connect to Server</button>
+      <h2>Recent Messages</h2>
+      <div className='recieved-messages'>{recievedMessage.map(message => (<h4>{message}</h4>))}</div>
+      <input className='input-bar' value={sendMessage} onChange={e => setSendMessage(e.target.value)}></input>
+      <button className='send-message-button' onClick={send}>Send</button>
+    </div>
+  );
+}
+
+export default TestPage;
