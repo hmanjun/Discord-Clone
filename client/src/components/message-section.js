1f8838afb8ec98c40f2c8af1bd77edf71323714c
hmanjun <harshith.hm03@gmail.com>
2023-01-31 20:01:57 -0800

    Testing sending room token

--- client/src/components/message-section.js
+++ client/src/components/message-section.js
@@ -24,7 +24,7 @@ const MessageSection = () => {
     useEffect(() => {
         async function fetch(){
             await axios
-                .get(`${process.env.REACT_APP_API_URL}/api/chat-room/room-data`, {headers: {'Authorization': `Bearer ${jwtService.getUserToken()}`}})
+                .get(`${process.env.REACT_APP_API_URL}/api/chat-room/room-data`, {headers: {'room': `Bearer ${jwtService.getUserToken()}`}})
                 .then(response => {
                     //console.log(response)
                     setMessageData([...response.data.messages])
